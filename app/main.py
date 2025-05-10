ba969518371857502f91bff7d9e840f8bc051864
mateus.fecassio <mateus.fecassio@gmail.com>
2025-05-10 13:00:55 -0300

    Add reset endpoint to main due to its simplicity for the final API. Correct repository name.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from app.infrastructure.repositories.account_repository import AccountRepository
 
 app = FastAPI(
     title="Ebanx Account API",
     description="API for Account Management.",
     version="1.0.0"
 )
+
+@app.post("/reset")
+def reset():
+    AccountRepository.reset()
+    return Response(content="OK", status_code=200)
